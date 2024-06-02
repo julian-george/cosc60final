a4e7fe214f1fb6b88bcca7220904392781f9b34e
Julian George <juliancgeorge@gmail.com>
2024-06-02 02:33:49 -0400

    implementing to the best of my ability RFC packet structures

--- quic.py
+++ quic.py
@@ -1,9 +1,10 @@
 from scapy.all import *
 from scapy.fields import *
+from scapy.layers import *
 
 
 # Base class for QUIC packets
-class QUIC(Packet):
+class QUICPacket(Packet):
     name = "QUIC"
     fields_desc = [
         BitEnumField("header_form", 1, 1, {0: "short", 1: "long"}),
@@ -14,23 +15,24 @@ class QUIC(Packet):
         if self.header_form == 1:  # Long Header
             return QUICLongHeader
         else:  # Short Header
-            return QUICShortHeader
+            return QUIC1RTT
 
     def extract_padding(self, s):
         return "", s
 
 
+# TODO: link this to packet_number_length
 PacketNumberField = XIntField("packet_number", 0)
 
 
-# Long header base class
-class QUICLongHeader(QUIC):
-    fields_desc = QUIC.fields_desc + [
-        BitEnumField(
-            "type", 0, 2, {0: "Initial", 1: "0-RTT", 2: "Handshake", 3: "Retry"}
-        ),
-        BitField("reserved", 0, 4),
-        XIntField("version", 0),
+# Payload should be integer list of supported QUIC versions
+# This is considered a "long header packet" but only shares length, not fields, with the rest of the category
+class QUICVersionNegotiation(QUICPacket):
+    fields_desc = QUICPacket.fields_desc + [
+        # Set to arbitrary value
+        BitField("unused", 0, 7),
+        # Must be zero for this packet type
+        BitField("version", 0, 32),
         XByteField("dcid_len", 0),
         XStrLenField("dcid", "", length_from=lambda pkt: pkt.dcid_len),
         XByteField("scid_len", 0),
@@ -38,18 +40,33 @@ class QUICLongHeader(QUIC):
     ]
 
 
-# Short header class
-class QUICShortHeader(QUIC):
-    fields_desc = QUIC.fields_desc + [
-        BitField("key_phase", 0, 1),
-        BitField("packet_number", 0, 7),
-        ConditionalField(
-            XStrFixedLenField("dcid", "", length=8), lambda pkt: pkt.key_phase == 1
+# Long header base class
+class QUICLongHeader(QUICPacket):
+    fields_desc = QUICPacket.fields_desc + [
+        BitEnumField(
+            "type", 0, 2, {0: "Initial", 1: "0-RTT", 2: "Handshake", 3: "Retry"}
         ),
+        ConditionalField(BitField("type_specific", 0, 4), lambda pkt: pkt.type == 3),
+        ConditionalField(BitField("reserved", 0, 2), lambda pkt: pkt.type != 3),
+        ConditionalField(
+            BitField("packet_number_length", 0, 2), lambda pkt: pkt.type != 3
+        ),
+        BitField("version", 0, 32),
+        XByteField("dcid_len", 0),
+        XStrLenField("dcid", "", length_from=lambda pkt: pkt.dcid_len),
+        XByteField("scid_len", 0),
+        XStrLenField("scid", "", length_from=lambda pkt: pkt.scid_len),
     ]
 
-
-# Specific packet types as subclasses of QUICLongHeader
+    def guess_payload_class(self, payload):
+        if self.type == 0:
+            return QUICInitial
+        elif self.type == 1:
+            return QUIC0RTT
+        elif self.type == 2:
+            return QUICHandshake
+        elif self.type == 3:
+            return QUICRetry
 
 
 class QUICInitial(QUICLongHeader):
@@ -61,39 +78,64 @@ class QUICInitial(QUICLongHeader):
     ]
 
     def __init__(self, *args, packet_number_length=0, **kwargs):
-        """
-        :param packet_number_length: one less than the packet number's size in bytes
-        """
-        if not 0 <= packet_number_length <= 3:
-            raise ValueError("packet_number_length must be a 2-bit value (0-3)")
         super(QUICInitial, self).__init__(*args, **kwargs)
         self.type = 0
-        self.reserved = packet_number_length
 
 
 class QUIC0RTT(QUICLongHeader):
-    def __init__(self, *args, **kwargs):
+    fields_desc = QUICLongHeader.fields_desc + [
+        XByteField("length", 0),
+        PacketNumberField,
+    ]
+
+    def __init__(self, *args, packet_number_length=0, **kwargs):
         super(QUIC0RTT, self).__init__(*args, **kwargs)
         self.type = 1
 
 
 class QUICHandshake(QUICLongHeader):
-    def __init__(self, *args, **kwargs):
-        super(QUICHandshake, self).__init__(*args, **kwargs)
+    fields_desc = QUICLongHeader.fields_desc + [
+        XByteField("length", 0),
+        PacketNumberField,
+    ]
+
+    def __init__(self, *args, packet_number_length=0, **kwargs):
+        super(QUIC0RTT, self).__init__(*args, **kwargs)
         self.type = 2
 
 
+# Has no payload
 class QUICRetry(QUICLongHeader):
+    fields_desc = QUICLongHeader.fields_desc + [
+        # Our implementation uses 128 bit retry tokens
+        XStrFixedLenField("retry_token", "", 16),
+        BitField("retry_integrity_tag", 0, 128),
+    ]
+
     def __init__(self, *args, **kwargs):
         super(QUICRetry, self).__init__(*args, **kwargs)
         self.type = 3
 
 
+# The only short header packet defined in the RFC
+class QUIC1RTT(QUICPacket):
+    fields_desc = QUICPacket.fields_desc + [
+        BitField("spin", 0, 1),
+        # Must be 0
+        BitField("reserved", 0, 2),
+        BitField("key_phase", 0, 1),
+        BitField("packet_number_length", 0, 2),
+        # TODO: what should be the length of this?
+        XStrFixedLenField("dcid", "", 20),
+        PacketNumberField,
+    ]
+
+
 # Binding layers
-bind_layers(UDP, QUIC, dport=443)
-bind_layers(UDP, QUIC, sport=443)
-bind_layers(QUIC, QUICLongHeader, header_form=1)
-bind_layers(QUIC, QUICShortHeader, header_form=0)
+bind_layers(UDP, QUICPacket, dport=443)
+bind_layers(UDP, QUICPacket, sport=443)
+bind_layers(QUICPacket, QUICLongHeader, header_form=1)
+bind_layers(QUICPacket, QUIC1RTT, header_form=0)
 bind_layers(QUICLongHeader, QUICInitial, type=0)
 bind_layers(QUICLongHeader, QUIC0RTT, type=1)
 bind_layers(QUICLongHeader, QUICHandshake, type=2)
@@ -101,11 +143,10 @@ bind_layers(QUICLongHeader, QUICRetry, type=3)
 
 # Test Example
 pkt = (
-    Ether()
-    / IP()
+    IP()
     / UDP()
     / QUICInitial(
-        version=1, dcid_len=4, dcid="abcd", scid_len=4, scid="1234", packet_number=1
+        version=1, dcid_len=4, dcid="abcd", scid_len=4, scid="1234", packet_number=900
     )
 )
 pkt.show()
