a65f5202072f99baad660c27f422e70e782adbf5
Julian George <juliancgeorge@gmail.com>
2024-06-05 19:05:07 -0400

    basic client

--- quic-client.py
+++ quic-client.py
@@ -1,8 +1,10 @@
 from scapy.all import *
 
+from quic_packets import *
+
 def main():
-    p = IP("192.168.64.8") / UDP(sport=10930, dport=443)
-    sr(p)
+    p = IP(dst="192.168.64.8") / UDP(sport=10930, dport=443) / QUICPacket()
+    send(p)
 
 
 if __name__ == "__main__":
